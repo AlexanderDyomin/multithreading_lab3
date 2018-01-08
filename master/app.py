d4bc628b76d922706a64f9a57464388b202459a8
Alexander Dyomin <dyomisasha@yandex.ru>
2018-01-08 15:42:13 +0300

    Modified to answer questions 1 and 3

--- master/app.py
+++ master/app.py
@@ -3,24 +3,33 @@ import redis
 import zmq
 app = Flask(__name__)
 
-conn = redis.Redis('redis')
-if(int(conn.llen("fib")) == 0):
-    conn.rpush("fib", 2 ,1)
-
 @app.route('/')
 def index():
-	context = zmq.Context()
-	socket = context.socket(zmq.REQ)
-	socket.connect("tcp://worker:5555")
-	#conn = redis.Redis('redis')
-	x1 = conn.lindex("fib", 0)
-	fib1 = int(x1)
-	m = "Current Fibonacci num  = " + str(x1)
-	socket.send('1')
-	curFib = socket.recv()
-	conn.lset("fib", 0, curFib)
-	conn.lset("fib", 1, fib1)
-	return m
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://worker:5555")
+    #conn = redis.Redis('redis')
+    x1 = conn.lindex("fib", 0)
+    fib1 = int(x1)
+    m = "Current Fibonacci num  = " + str(x1)
+    socket.send('next') # dumb message just to send to worker and make it work
+    answer = socket.recv()
+    #conn.lset("fib", 0, curFib)
+    #conn.lset("fib", 1, fib1)
+    return m
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", debug=True)
+    conn = redis.Redis('redis')
+    # to prevent app start before Redis is up
+    while 1:
+        try:
+            # print("trying to connect")
+            conn.ping()
+            break
+        except redis.ConnectionError:
+            continue
+    
+    if(int(conn.llen("fib")) == 0):
+        conn.rpush("fib", 2 ,1)
+
+    app.run(host="0.0.0.0", debug=True)
--- worker/app.py
+++ worker/app.py
@@ -1,20 +1,36 @@
 import redis
-import zmq 
+import zmq
 
-def fib():
-	conn = redis.Redis('redis')
-	x2 = conn.lindex("fib", 1)
-	x1 = conn.lindex("fib", 0)
-	fib1 = int(x1)
-	fib2 = int(x2)
-	curFib = fib1 + fib2
-	strCurFib = str(curFib)
-	return strCurFib
-
+def fib(conn):
+    pipe = conn.pipeline()
+    while 1:
+        try:
+            # print("inside fib")
+            pipe.watch("fib")
+            x2 = pipe.lindex("fib", 1)
+            x1 = pipe.lindex("fib", 0)
+            fib1 = int(x1)
+            fib2 = int(x2)
+            curFib = fib1 + fib2
+            pipe.multi()
+            pipe.lset("fib", 0, curFib)
+            pipe.lset("fib", 1, fib1)
+            pipe.execute()
+            break
+            #strCurFib = str(curFib)
+            #return strCurFib
+        except redis.WatchError:
+            continue
+        finally:
+            pipe.reset()
+    return
+            
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
+conn = redis.Redis('redis')
 while True:
-	mes = socket.recv()
-	curFib = fib()
-	socket.send(curFib)
+    mes = socket.recv() # recieved dumb message, time to work
+    # print("worker recieved signal from master")
+    fib(conn)
+    socket.send('done')
